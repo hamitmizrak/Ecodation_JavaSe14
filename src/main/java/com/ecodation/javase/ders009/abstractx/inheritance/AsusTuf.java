8b023bb31ecebd4a5929a123bd833d00c5acb98a
hamitmizrak <hamitmizrak@gmail.com>
2022-01-16 11:25:37 +0300

    içiçe extends

---
+++ src/main/java/com/ecodation/javase/ders009/abstractx/inheritance/AsusTuf.java
@@ -0,0 +1,9 @@
+package com.ecodation.javase.ders009.abstractx.inheritance;
+
+//Formal= Ctrl+Alt+l
+public class AsusTuf extends Asus {
+    public static void main(String[] args) {
+        AsusTuf asusTuf=new AsusTuf();
+        asusTuf.setCpu("44");
+    }
+}
